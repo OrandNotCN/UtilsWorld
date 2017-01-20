c5944cb8780290c4dbac8d7a4a4cd2a2dc536a79
zlc <zhoulianchun@foxmail.com>
2017-01-20 15:22:00 +0800

    微信抢红包

--- wxhb/src/main/java/com/orandnot/wxhongbao/MainActivity.java
+++ wxhb/src/main/java/com/orandnot/wxhongbao/MainActivity.java
@@ -1,9 +1,11 @@
 package com.orandnot.wxhongbao;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -14,6 +16,10 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    Switch swAu;
+    Switch sws;
+    Switch swv;
+    Spinner spd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +28,30 @@ public class MainActivity extends Activity {
         init();
     }
 
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        swAu.setChecked(RobService.isAuto);
+        sws.setChecked(RobService.isShake);
+        swv.setChecked(RobService.isVoice);
+        spd.setSelection(RobService.delay);
+        TextView tapp = (TextView) findViewById(R.id.app);
+        if (CommonUtils.isHaveVersion(this)) {
+            tapp.setText("当前微信版本号：" + CommonUtils.getVersion(this, "com.tencent.mm"));
+            if (!CommonUtils.isAccessibilitySettingsOn(this, RobService.class)) {
+                showDialogTip();
+            }
+        } else {
+            findViewById(R.id.rls).setVisibility(View.GONE);
+            tapp.setText("不支持当前微信版本" + CommonUtils.getVersion(this, "com.tencent.mm"));
+        }
+
+    }
+
     private void init() {
-        Switch swAu = (Switch) findViewById(R.id.swAu);
+        swAu = (Switch) findViewById(R.id.swAu);
         swAu.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
@@ -32,7 +60,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        Switch sws = (Switch) findViewById(R.id.sws);
+        sws = (Switch) findViewById(R.id.sws);
         sws.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
@@ -41,7 +69,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        Switch swv = (Switch) findViewById(R.id.swv);
+        swv = (Switch) findViewById(R.id.swv);
         swv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
@@ -50,7 +78,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        Spinner spd = (Spinner) findViewById(R.id.spde);
+        spd = (Spinner) findViewById(R.id.spde);
         spd.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
@@ -65,16 +93,6 @@ public class MainActivity extends Activity {
         });
 
 
-        TextView tapp = (TextView) findViewById(R.id.app);
-        if (CommonUtils.isHaveVersion(this)) {
-            tapp.setText("当前微信版本号：" + CommonUtils.getVersion(this, "com.tencent.mm"));
-            if (!CommonUtils.isAccessibilitySettingsOn(this, RobService.class)) {
-                showDialogTip();
-            }
-        } else {
-            findViewById(R.id.rls).setVisibility(View.GONE);
-            tapp.setText("不支持当前微信版本" + CommonUtils.getVersion(this, "com.tencent.mm"));
-        }
     }
 
     private void showDialogTip() {
